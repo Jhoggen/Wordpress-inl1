6e2368bcb024c763299465e0b5cd9b3498b23340
Jhoggen <jacob.hoggen@medieinstitutet.se>
2022-04-11 09:20:52 +0200

    index.php
    startsida

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+<main>
+			<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="hero">
+								<?php the_post_thumbnail(); ?>
+								<div class="text">
+                                <?php
+                                    if( have_posts() ) {
+
+                                        while( have_posts() ) {
+
+                                            the_post();
+                                            the_content();
+                                        }
+                                    }
+                                ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+
+<?php get_footer(); ?>
